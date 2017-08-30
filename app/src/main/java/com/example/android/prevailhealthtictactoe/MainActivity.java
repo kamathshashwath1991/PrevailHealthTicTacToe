df0a2467d55d31c9182888f01530601dcc9a2c5d
kamathshashwath1991 <kamathshashwath1991@gmail.com>
2017-08-30 12:39:47 -0500

    Dimens added

--- app/src/main/java/com/example/android/prevailhealthtictactoe/MainActivity.java
+++ app/src/main/java/com/example/android/prevailhealthtictactoe/MainActivity.java
@@ -11,38 +11,39 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private int activePlayer= 0;
-    private int[] gameState={2,2,2,2,2,2,2,2,2};
-    private int[][] winningPositions= {{0,1,2},{3,4,5},{6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+    private int Player = 0;
+    private int[] DefaultGameState={2,2,2,2,2,2,2,2,2};
+    private int[][] winningPositions= {{0,1,2},{3,4,5},{6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};// define positions of winning
     private boolean gameIsActive= true;
 
+
+
     public void dropIn(View view){
-
         ImageView counter= (ImageView) view;
-        int tappedCounter= Integer.parseInt(counter.getTag().toString());
+        int imageTapCount= Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter]==2 && gameIsActive){
-            gameState[tappedCounter] = activePlayer;
+        if (DefaultGameState[imageTapCount]==2 && gameIsActive){
+            DefaultGameState[imageTapCount] = Player;
             counter.setTranslationY(-1000f);
 
-            if (activePlayer==0){
+            if (Player ==0){
                 counter.setImageResource(R.drawable.yellow);
-                activePlayer=1;
+                Player =1;
             }
             else {
                 counter.setImageResource(R.drawable.red);
-                activePlayer=0;
+                Player =0;
             }
             counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
             for (int[] winningPosition: winningPositions){
-                if (gameState[winningPosition[0]]==gameState[winningPosition[1]] &&
-                gameState[winningPosition[1]]==gameState[winningPosition[2]] &&
-                gameState[winningPosition[0]]!= 2){
-                    Log.d(TAG, "gameState: " +gameState[winningPosition[0]]);
+                if (DefaultGameState[winningPosition[0]]==DefaultGameState[winningPosition[1]] &&
+                DefaultGameState[winningPosition[1]]==DefaultGameState[winningPosition[2]] &&
+                DefaultGameState[winningPosition[0]]!= 2){
+                    Log.d(TAG, "gameState: " +DefaultGameState[winningPosition[0]]);
 
                     String winner= "Red";
-                    if (gameState[winningPosition[0]]==0){
+                    if (DefaultGameState[winningPosition[0]]==0){
                         winner= "Yellow";
                     }
 
@@ -53,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else {
                     boolean gameIsOver = true;
-                    for (int counterState: gameState){
+                    for (int counterState: DefaultGameState){
                         if (counterState==2){
                             gameIsOver= false;
                         }
@@ -74,9 +75,9 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout linearLayout= (LinearLayout) findViewById(R.id.layout_playAgain);
         linearLayout.setVisibility(View.INVISIBLE);
 
-        activePlayer=0;
-        for (int i=0; i< gameState.length; i++){
-            gameState[i]= 2;
+        Player =0;
+        for (int i=0; i< DefaultGameState.length; i++){
+            DefaultGameState[i]= 2;
         }
 
         GridLayout gridLayout= (GridLayout) findViewById(R.id.gridLayout);
